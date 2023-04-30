0a7839f6e86e69408a75452aac11c92c67386a41
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-30 08:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function func() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve('Hello, world!')
+		}, 1000)
+	})
+}
+
+func().then((value) => {
+	document.getElementById('output').innerText = value;
+})
